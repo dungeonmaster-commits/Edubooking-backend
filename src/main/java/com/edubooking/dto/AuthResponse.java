c5c4818c2e7403effcbce9cc563218ca512af090
dungeonmaster-commits <jointfamily307a@gmail.com>
2025-12-09 10:21:49 +0530

    added test depenmdencies in pom.xml

--- src/main/java/com/edubooking/dto/AuthResponse.java
+++ src/main/java/com/edubooking/dto/AuthResponse.java
@@ -3,11 +3,13 @@ package com.edubooking.dto;
 import com.edubooking.model.Role;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class AuthResponse {
     private String token;
     private Long userId;
--- src/main/java/com/edubooking/model/User.java
+++ src/main/java/com/edubooking/model/User.java
@@ -1,6 +1,7 @@
 package com.edubooking.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "users")
 public class User {
 
